14d71dc1a883e4800505d5f537d4fa393dc3ed5d
areyn77463 <ajr2934@outlook.com>
2023-01-08 15:24:13 -0500

    added alert for mobile users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    alert("This is not optimized to run on mobile devices yet.")
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
